863aa93b93b18ccd1a9a583788158d4b53b117bd
IamNagas <92800790+IamNagas@users.noreply.github.com>
2022-04-20 15:40:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+ //import Registration from './Registration/Registration' 
+//import Registartion from './Registration/funcomp/Registration'
+//import Todoform from './Todoform/Todoform'
+//import Todolist from './Todoform/Todolist'
+//import NewTodo from './NewTodo/NewTodo';
+//import Practice from './practiceT/TF';
+//import Validationform from './Validation/Validationform';
+import Chart from './Chart/chart';
+class App extends Component {
+  render() {
+    return (
+      <>
+      <Chart/>
+      </>
+
+    )
+  }
+}
+
+export default App;
